43f5169953350fd30190fd3a73344eddb5e4b5f9
Divesh Singh <diveshsingh633@gmail.com>
2025-07-18 20:07:08 +0530

    Added backend

---
+++ backend/contollers/userController.js
@@ -0,0 +1,68 @@
+import userModel from "../modals/userModal.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import validator from "validator";
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.json({ success: false, message: "User Doesn't Exist" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.json({ success: false, message: "Invalid Credentials" });
+    }
+
+    const token = createToken(user._id);
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+const registerUser = async (req, res) => {
+  const { username, password, email } = req.body;
+
+  try {
+    const exists = await userModel.findOne({ email });
+    if (exists) {
+      return res.json({ success: false, message: "User Already Exists" });
+    }
+
+    if (!validator.isEmail(email)) {
+      return res.json({ success: false, message: "Please Enter a valid Email" });
+    }
+
+    if (password.length < 8) {
+      return res.json({ success: false, message: "Please Enter a Strong Password" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new userModel({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    const token = createToken(newUser._id);
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+export { loginUser, registerUser };
